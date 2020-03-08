3d36bd788942d573c3f4c4e41c36661d7106b60b
RytisDr <r.drazdauskas@gmail.com>
2020-03-08 23:20:02 +0100

    Starts work on my locations page.

--- src/pages/MyLocations.jsx
+++ src/pages/MyLocations.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 
 export default class MyLocations extends Component {
-  state = {};
-  /*     if (window.localStorage.length) {
-      let keys = Object.keys(localStorage);
+  state = {
+    cities: []
+  };
+  componentDidMount() {
+    let keys = Object.keys(localStorage);
+    if (keys) {
+      console.log(keys);
       for (let key of keys) {
-        //console.log(`${key}: ${localStorage.getItem(key)}`);
-        this.fetchCurrentWeather(undefined, key);
+        const parseKey = parseInt(key);
+
+        if (Number.isInteger(parseKey)) {
+          //this.fetchCities(key);
+          // console.log(key);
+        }
       }
-    } */
+      //this.setState({ cities: cityIdArr });
+    }
+  }
+  fetchCities = id => {
+    const APIKey = "ed63ab6e517fbb4dbbf4444d70356ca0";
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${APIKey}&units=metric`
+    )
+      .then(res => res.json())
+      .then(city => {
+        let joined = this.state.cities.concat(city);
+        this.setState({ cities: joined });
+      });
+  };
+
   render() {
-    return <div>{}</div>;
+    const cities = this.state.cities;
+    return (
+      <div className="myLocationsWrap">
+        {cities.map(city => {
+          // console.log(city);
+        })}
+      </div>
+    );
   }
 }
